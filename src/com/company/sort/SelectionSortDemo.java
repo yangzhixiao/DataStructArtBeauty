8cf5f25acac52c155c09b07b190596271d7eb607
yangzhixiao <yangzhixiao1989@foxmail.com>
2018-10-17 09:15:14 +0800

    冒泡排序，插入排序，选择排序

--- src/com/company/sort/SelectionSortDemo.java
+++ src/com/company/sort/SelectionSortDemo.java
@@ -23,10 +23,28 @@ public class SelectionSortDemo {
         }
     }
 
+    public static void selectionSort2(int[] arr) {
+        if (arr.length <= 1) {
+            return;
+        }
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[i]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = new int[] { 4, 3, 7, 6, 9, 2, 1, 8, 5, 0 };
         BubbleSortDemo.printArr(arr);
         selectionSort(arr);
+        BubbleSortDemo.printArr(arr);
+        selectionSort2(arr);
         BubbleSortDemo.printArr(arr);
     }
 
