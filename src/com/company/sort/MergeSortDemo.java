989ac3f50d33982bc22cad22b2d4562863e6b90d
yangzhixiao <yangzhixiao1989@foxmail.com>
2018-10-17 11:26:26 +0800

    归并排序

--- src/com/company/sort/MergeSortDemo.java
+++ src/com/company/sort/MergeSortDemo.java
@@ -5,63 +5,67 @@ package com.company.sort;
  */
 public class MergeSortDemo {
 
-    public static int[] mergeSort(int[] arr, int q, int r) {
+    public static void mergeSort(int[] arr, int q, int r) {
 
         // 当前片段总数
         int n = r - q + 1;
 
         if (n <= 1) {
-            return new int[] { arr[q] };
+            return;
         }
 
         if (n == 2) {
             if (arr[r] < arr[q]) {
-                return new int[] { arr[r], arr[q] };
-            } else {
-                return new int[] { arr[q], arr[r] };
+                int temp = arr[r];
+                arr[r] = arr[q];
+                arr[q] = temp;
             }
+            return;
         }
 
         // 分割点
         int m = n / 2;
 
-        int[] arrLeft = mergeSort(arr, q, q + m - 1);
-        int[] arrRight = mergeSort(arr, q + m, r);
+        mergeSort(arr, q, q + m - 1);
+        mergeSort(arr, q + m, r);
 
         int[] temp = new int[n];
 
-        int i = 0, j = 0, k = 0;
+        int i = q, j = q + m, k = 0;
         while (k < n) {
-            if (i == arrLeft.length) {
-                temp[k] = arrRight[j];
-                j++;
+            // 左边已经没有元素，之间拷贝右边
+            if (i > q + m - 1) {
+                temp[k] = arr[j++];
                 k++;
                 continue;
             }
-            if (j == arrRight.length) {
-                temp[k] = arrLeft[i];
-                i++;
+            // 右边已经没有元素，之间拷贝左边
+            if (j > r) {
+                temp[k] = arr[i++];
                 k++;
                 continue;
             }
-            if (arrLeft[i] < arrRight[j]) {
-                temp[k] = arrLeft[i];
+            if (arr[i] < arr[j]) {
+                temp[k] = arr[i];
                 i++;
                 k++;
             } else {
-                temp[k] = arrRight[j];
+                temp[k] = arr[j];
                 j++;
                 k++;
             }
         }
 
-        return temp;
+        // 复制到原来q到r之间
+        for (int index = 0; index < n; index++) {
+            arr[q + index] = temp[index];
+        }
     }
 
     public static void main(String[] args) {
         int[] arr = new int[] { 4, 3, 7, 6, 9, 2, 1, 8, 5, 0 };
         BubbleSortDemo.printArr(arr);
-        arr = mergeSort(arr, 0, arr.length - 1);
+        mergeSort(arr, 0, arr.length - 1);
         BubbleSortDemo.printArr(arr);
     }
 
